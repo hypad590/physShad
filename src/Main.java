540bf9a4361cd4c02f52a13c2521a98914f19579
hypad590 <aloxaryabov@gmail.com>
2024-01-25 19:19:08 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public static JFrame window;
+    public static JLabel label;
+    private static int w= 800;
+    private static int h = 400;
+    public float angle;
+
+    public static void main(String[] args) {
+        window = new JFrame("physShad");
+        window.setBounds(0,0,w,h);
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setLayout(null);
+        ImageIcon bgimg = new ImageIcon("D:/projects/jav/physShadows/src/bg.jpg");
+        label = new JLabel(bgimg);
+        label.setLayout(new BorderLayout());
+        label.setBounds(window.getBounds());
+        window.setContentPane(label);
+        window.setVisible(true);
+    }
 }
