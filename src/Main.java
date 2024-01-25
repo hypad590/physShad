9ee2c69aabec86da281ac6a8332248476624be2b
hypad590 <aloxaryabov@gmail.com>
2024-01-25 19:38:27 +0300

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
-class VertLine extends JComponent{
+
+class VertLine extends JComponent {
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.cyan);
-        int x = getWidth() /3;
-        g.drawLine(x,0,x, getHeight());
+        g.setColor(Color.CYAN);
+        int x = getWidth() / 3;
+        g.drawLine(x, 0, x, getHeight());
     }
 }
+
 public class Main extends JFrame {
 
     public static JFrame window;
     public static JLabel label;
     public static ImageIcon bgimg;
     public static JPanel panel;
-    private static int w= 800;
+    private static int w = 800;
     private static int h = 400;
-    public float angle;
 
     public static void main(String[] args) {
-        window = new JFrame("physShad");
-        window.setBounds(0,0,w,h);
-        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        window.setLayout(null);
-        panel= new JPanel(new BorderLayout());
-        bgimg = new ImageIcon("D:/projects/jav/physShadows/src/bg.png");
-        label = new JLabel(bgimg);
-        label.setSize(new Dimension(w,h));
-        panel.add(label, BorderLayout.CENTER);
-        VertLine line = new VertLine();
-        panel.add(line);
-        window.setContentPane(panel);
-        window.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            window = new JFrame("physShad");
+            window.setBounds(0, 0, w, h);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            panel = new JPanel(null);
+            bgimg = new ImageIcon("D:/projects/jav/physShadows/src/bg.png");
+            label = new JLabel(bgimg);
+            label.setSize(new Dimension(w, h));
+            panel.add(label);
+
+            VertLine line = new VertLine();
+            line.setSize(1, h);
+            line.setLocation(w / 3, 0);
+            panel.add(line);
+
+            window.setContentPane(panel);
+            window.setVisible(true);
+        });
     }
 }
