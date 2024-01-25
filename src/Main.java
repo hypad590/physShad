6eb3f57d1eb0624d4aa0237c621a5434eb538098
hypad590 <aloxaryabov@gmail.com>
2024-01-25 19:34:42 +0300

    Draw

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+class VertLine extends JComponent{
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
 
+        g.setColor(Color.cyan);
+        int x = getWidth() /3;
+        g.drawLine(x,0,x, getHeight());
+    }
+}
 public class Main extends JFrame {
 
     public static JFrame window;
     public static JLabel label;
+    public static ImageIcon bgimg;
+    public static JPanel panel;
     private static int w= 800;
     private static int h = 400;
     public float angle;
@@ -14,11 +25,14 @@ public class Main extends JFrame {
         window.setBounds(0,0,w,h);
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
         window.setLayout(null);
-        ImageIcon bgimg = new ImageIcon("D:/projects/jav/physShadows/src/bg.jpg");
+        panel= new JPanel(new BorderLayout());
+        bgimg = new ImageIcon("D:/projects/jav/physShadows/src/bg.png");
         label = new JLabel(bgimg);
-        label.setLayout(new BorderLayout());
-        label.setBounds(window.getBounds());
-        window.setContentPane(label);
+        label.setSize(new Dimension(w,h));
+        panel.add(label, BorderLayout.CENTER);
+        VertLine line = new VertLine();
+        panel.add(line);
+        window.setContentPane(panel);
         window.setVisible(true);
     }
 }
